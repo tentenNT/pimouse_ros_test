8c1ba02f9bc3cb22c44b9ccd9699b28905433dbf
tentenNT <takuminpc@gmail.com>
2019-12-09 19:57:55 +0900

    add bash

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -xve
+
+#required packages
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
+
+#ros install
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu18.04_server.git
+cd ./ros_setup_scripts_Ubuntu18.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+catkin_make
+source devel/setup.bash
+
